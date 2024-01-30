90a3f80756de862240a536a6d39298e083017feb
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-01-30 18:59:00 +0000

    0-task

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,4 @@
 #!/usr/bin/env ruby
 
-#check if arguments are provided
-if ARGV.empty?
-  puts "Usage: ./0-simply_match_school.rb <input>"
-  exit 1
-end
-
-#Extact arguments provided
-input = ARGV[0]
-
-# Define the regular expression
-regex = /School/
-
-# Use the match method to find matches
-match = input.scan(regex)
-
-# Output results
-if match.empty?
-  puts "$"
-else
-  puts match.join + "$"
-end
+#check if arguments are the same 
+puts ARGV[0].scan(/School/).join
