889e7bd7907bf848469f076c30f731cc7d19e3cd
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-05-07 21:59:16 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-import requests
+"""task 0"""
 
 
 def number_of_subscribers(subreddit):
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        headers = {'User-Agent': 'My-User-Agent'}
-        sub_info = requests.get(url, headers=headers, allow_redirects=False)
+    """Queries and returns the number of subscribers
+    """
+    import requests
 
-        if sub_info.status_code >= 300:
-            return 0
-            
-        return sub_info.json().get("data").get("subscribers")
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
 
-
-if __name__ == "__main__":
-    import sys
+    return sub_info.json().get("data").get("subscribers")
