0332554981b0b138521a3f3c30b19ca3056c5e63
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-05-08 21:10:02 +0300

    done agaiiiin

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,12 @@
 # Create a manifest that fixes file name typo
 exec { 'fix_apache_config':
-  command     => '/bin/sed -i "s/DirectoryIndex.*/DirectoryIndex index.php index.html/g" /etc/apache2/mods-enabled/dir.conf',
+  command     => '/bin/sed -i "s/DirectoryIndex.*/DirectoryIndex index.php index.html/g" /etc/apache2/mods-available/dir.conf && /bin/ln -sf /etc/apache2/mods-available/dir.conf /etc/apache2/mods-enabled/dir.conf',
+  path        => '/bin',
   refreshonly => true,
-  subscribe   => File['/etc/apache2/mods-enabled/dir.conf'],
 }
 
-service { 'apache2':
-  ensure => running,
-  enable => true,
+service { 'httpd':
+  ensure  => running,
+  enable  => true,
+  require => Exec['fix_apache_config'],
 }
