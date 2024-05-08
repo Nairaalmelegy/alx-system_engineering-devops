3ff114f8295c40bafd1b1b7628dcb0be711f94c6
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-05-08 21:03:24 +0300

    booooored

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,11 @@
 # Create a manifest that fixes file name typo
-exec { 'fix_typo':
-       command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
-                    path    => '/bin/'
+exec { 'fix_apache_config':
+  command     => '/bin/sed -i "s/DirectoryIndex.*/DirectoryIndex index.php index.html/g" /etc/apache2/mods-enabled/dir.conf',
+  refreshonly => true,
+  subscribe   => File['/etc/apache2/mods-enabled/dir.conf'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
